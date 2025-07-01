ff19d154af57214146ce27d1de413ce0691dc1c3
Yin Guo Wei <yinguowei@gmail.com>
2025-07-01 21:36:01 +0800

    day1, first commit. can batch rename files.

---
+++ README.md
@@ -0,0 +1,58 @@
+# SRM Export
+
+Rename [SRM (Steam ROM Manager)](https://github.com/SteamGridDB/steam-rom-manager) exported image file names like `9942969592307515392.tall.jpg` to normal game title file names like `Yoshi's Cookie (USA, Europe).jpg`.
+
+## Overview
+
+When exporting images using SRM, the file names are like `9942969592307515392.tall.jpg`.
+
+SRM also create a `_selections.json` data file with: `"title": "Yoshi's Cookie (USA, Europe)"`, `"filename": "9942969592307515392.tall.jpg"`, for example:
+
+```json
+[
+  {
+    "title": "Yoshi's Cookie (USA, Europe)",
+    "images": {
+      "tall": {
+        "pool": "Yoshi's Cookie",
+        "filename": "9942969592307515392.tall.jpg"
+      },
+      "long": {
+        "pool": "Yoshi's Cookie",
+        "filename": "9942969592307515392.long.png"
+      },
+      "hero": {
+        "pool": "Yoshi's Cookie",
+        "filename": "9942969592307515392.hero.png"
+      },
+      "logo": {
+        "pool": "Yoshi's Cookie",
+        "filename": "9942969592307515392.logo.png"
+      },
+      "icon": {
+        "pool": "Yoshi's Cookie",
+        "filename": "9942969592307515392.icon.png"
+      }
+    }
+  },
+  ...
+]
+```
+
+This program copies `9942969592307515392.tall.jpg` to the directory `Imgs` with the new file name `Yoshi's Cookie (USA, Europe).jpg` according to the json file content.
+
+## Usage
+
+Drag `_selections.json` file or its directory onto the program icon (on Windows), and wait for the command line window to prompt completion.
+
+Or open the program directly in console (on Windows and MacOS) and input the path of the json file or directory.
+
+The program will create a directory `Imgs` under the source `_selections.json` directory and copy the images with the correct file names to this directory.
+
+## Tips
+
+- If the `Imgs` directory exists and is not empty, the program will report an error, please clear it and retry.
+- If the original image file set in json does not exist, the program will prompt and continue, please check. Normally, SRM will not appear in this situation.
+- By default, the program copies the image to the `Imgs` directory under the directory where `_selections.json` is located, which can be modified in `config.json`'s `TARGET_DIR` field. And relative paths such as `../Imgs`, `./Imgs` are supported. And absolute paths are supported too, such as `C:\\Users\\username\\Imgs` (Windows) or `/Users/username/Imgs` (MacOS)
+- By default, the program will copy `*.tall.jpg` or `png` image, you can modify `config.json`'s `IMG_TYPE` to set other types, such as `"tall", "hero", "long", "icon"`, but `"tall"` is recommended.
+- And can add suffix to new image file, such as EmuELEC will uses `-image.png`, it will generate file: `Yoshi's Cookie (USA, Europe)-image.jpg`, modify `config.json`'s filed `IMG_SUFFIX` to add suffix. No suffix by default.
---
+++ srm-export.py
@@ -0,0 +1,100 @@
+import json
+import os
+import shutil
+import sys
+
+config_file = "./config.json"
+
+# if config file not exists, create default config json file
+if not os.path.exists(config_file):
+    configs = {
+        "TARGET_DIR": "./Imgs/",
+        "IMG_TYPE": "tall",
+        "IMG_SUFFIX": ""
+    }
+    json.dump(configs, open(config_file, "w"))
+
+# load configs
+configs = json.load(open(config_file))
+target_dir = configs.get("TARGET_DIR", "./Imgs/")
+img_type = configs.get("IMG_TYPE", "tall")  # support "tall", "hero", "long", "icon"
+img_suffix = configs.get("IMG_SUFFIX", "")  # or "-image", if you want file like: "Super Mario Bros. (USA) (Rev 1)-image.png
+
+# get json file path from command line argument
+if len(sys.argv) > 1:
+    json_file = sys.argv[1].strip()
+else:
+    # if no command line argument, prompt user to input path
+    json_file = input("Please input json file (or directory) path: ").strip()
+
+# if json file not exists, exit
+if not os.path.exists(json_file):
+    print(f"Input file not exists: {json_file}")
+    sys.exit(1)
+
+# if input file is not `_selections.json`, exit
+if os.path.isfile(json_file) and os.path.basename(json_file) != "_selections.json":
+    print(f"json file must be `_selections.json`, selected: {json_file}")
+    sys.exit(1)
+
+# if json file is a directory, find "_selections.json" file in it
+if os.path.isdir(json_file):
+    json_file = os.path.join(json_file, "_selections.json")
+    # if "_selections.json" not exists, exit
+    if not os.path.exists(json_file):
+        print(f"json file not exists: {json_file}")
+        sys.exit(1)
+
+# json_file = "./_selections.json"
+# test_json = "/Users/yinguowei/Roms (tiny-best-set-go)/GB/srm-image-choices/_selections.json"
+
+source_dir = os.path.dirname(os.path.abspath(json_file))
+target_dir = os.path.normpath(os.path.join(source_dir, target_dir))
+
+if not os.path.exists(target_dir):
+    os.makedirs(target_dir)
+elif os.listdir(target_dir):
+    print(f"target dir is not empty, please check and clean it: {target_dir}")
+    sys.exit(1)
+
+
+print(f"source dir: {source_dir}, target dir: {target_dir}")
+
+count = 0
+
+with open(json_file) as f:
+    data = json.load(f)
+
+    for item in data:
+        # get title and filename
+        title = item.get("title")
+        filename = item.get("images", {}).get(img_type, {}).get("filename", None)
+        if not filename:
+            continue
+
+        # if source file not exists, skip
+        source_file = os.path.join(source_dir, filename)
+        if not os.path.exists(source_file):
+            print(f"Warning! Source file not exists: {source_file}")
+            continue
+        ext = os.path.splitext(filename)[1]
+
+        # set target file path and file name
+        target_filename = title + img_suffix + ext
+        target_file = os.path.join(target_dir, target_filename)
+
+        # if target file already exists, skip
+        if os.path.exists(target_file):
+            print(f"Warning! Target file already exists: {target_file}")
+            continue
+
+        # copy file
+        try:
+            shutil.copy2(source_file, target_file)
+            print(f"copied file: \"{filename}\", to: \"{target_filename}\"")
+            count += 1
+        except Exception as e:
+            print(f"Error!!! when copying file: \"{filename}\", to: \"{target_filename}\": {e}")
+            continue
+
+    print(f"Done! Total copied files: {count}")
